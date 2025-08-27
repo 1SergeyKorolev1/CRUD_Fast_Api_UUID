089f083253fc8bc6c1c1b0294c747446ea3b0f95
Sergey Korolev <bongrichard@rambler.ru>
2025-08-27 23:43:56 +0300

    Убрал лишнее

--- services/services.py
+++ services/services.py
@@ -2,7 +2,6 @@ from models.models import Task
 from sqlalchemy.orm import Session
 from schemas.schemas import TaskCreate
 from fastapi import HTTPException
-import psycopg2
 
 def create_task(db: Session, data: TaskCreate):
     try:
@@ -21,7 +20,6 @@ def get_task(db: Session, task_uuid: str):
     try:
         task_object = db.query(Task).filter(Task.uuid == task_uuid).first()
         if task_object:
-            print(task_object)
             return task_object
         raise HTTPException(status_code=400, detail=f"неверный UUID")
     except Exception as e:
